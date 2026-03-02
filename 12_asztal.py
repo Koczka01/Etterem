22ffa585a8bfb5a9ccd317bd747ff8f59db8b501
dorottyavarsanyi <dorottyavarsanyi5@gmail.com>
2026-03-02 13:53:31 +0100

    asztal updated

--- 12_asztal.py
+++ 12_asztal.py
@@ -1,2 +1,9 @@
 tables_number = int(input("Adja, meg hogy hány asztalt szeretne használni: "))
+tables = []
 
+
+for i in range(tables_number):
+    add = []
+    tables.append(lo_siento)
+
+print(tables)
