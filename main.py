a619b9e384d6322dbeb3f7b8284be5e611bd1783
skorvanlorant-sys <skorvan.lorant@gmail.com>
2026-03-09 13:54:53 +0100

    Dominik butaságokat beszél neked

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 #Wilson! Lo siento!
-
+class Recipe():
+    def __init__(self, name):
+        self.name = name
+        self.material = []
+        self.amount = []
+    
 with open('menu.csv', encoding='utf8') as file:
     menu = []
     for i in file:
         menu.append(i.strip().split(';'))
 
 with open('recept.csv', encoding='utf8') as file:
+    read = file.readline()
+    read = read.strip().split(';')
     recipe = []
+    recipe.append(Recipe(read[0]))
+    recipe
+    print(recipe[0].name)
     for i in file:
-        recipe.append(i.strip().split(';'))
+        if i[0] != recipe[-1].name:
+            recipe.append(Recipe(i[0]))
 
 with open('raktar.csv', encoding='utf8') as file:
     storage = []
