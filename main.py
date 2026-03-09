a6ddda8db389b8e6cccd3f7270a0f5b970e6050f
dorottyavarsanyi <dorottyavarsanyi5@gmail.com>
2026-03-09 13:26:20 +0100

    I tried so hard

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 #Wilson! Lo siento!
 
+with open('menu.csv', encoding='utf8') as file:
+    menu = []
+    for i in file:
+        menu.append(i.strip().split(';'))
+
+with open('recept.csv', encoding='utf8') as file:
+    recipe = []
+    for i in file:
+        recipe.append(i.strip().split(';'))
+
+with open('raktar.csv', encoding='utf8') as file:
+    storage = []
+    for i in file:
+        storage.append(i.strip().split(';'))
+
+print(menu)
+print(recipe)
+print(storage)
