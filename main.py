77d765d3b15d642cd2636cee9248476aa612759c
skorvanlorant-sys <skorvan.lorant@gmail.com>
2026-03-23 14:07:11 +0100

    Yes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class table:
 
 tables = [table()]*table_count
 
-action = input('írd be, hogy new_recipe: ')
+action = input('írd be, hogy new_recipe vagy Storage_load: ')
 while action != '':
     if action == 'new_recipe':
         read_files.recipe.append(read_files.Recipe(input('Add meg a nevét a kajának: ')))
@@ -27,10 +27,17 @@ while action != '':
 
             material = input('Adjon meg egy alapanyagot, ha nem szeretne többet, akkor nyomjon entert: ')
 
-    write_files.menu(read_files.recipe[-1].name)
+        write_files.menu(read_files.recipe[-1].name)
 
-    write_files.recipe(read_files.recipe[-1].name, read_files.recipe[-1].material, read_files.recipe[-1].amount)
+        write_files.recipe(read_files.recipe[-1].name, read_files.recipe[-1].material, read_files.recipe[-1].amount)
 
-    action = input('Nyomj egy entert: ')
+    if action == "Storage_load":
+        i = 0
+        m = input("kérem adja meg, hogy mit szeretne feltölteni: ")
+        while i < len(read_files.storage) and m == read_files.storage[i][0]:
+            read_files.storage.append(read_files.Recipe(input("Kérlek add meg, hogy mennyi szeretnél hozzá adni: ")))
+            i += 1
+
+    action = input('Nyomj egy entert: ')    
 
 print(read_files.recipe[-1].amount)
